82488c01937591b3feaafb23b2f503e5670f3bdf
Panbers <56555723+Panbers@users.noreply.github.com>
2025-11-05 20:59:10 -0300

    Update supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+  const SUPABASE_URL = "https://znlontctluxtipvrmguq.supabase.co";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpubG9udGN0bHV4dGlwdnJtZ3VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNzMzMjIsImV4cCI6MjA3Nzk0OTMyMn0.nlRzQ9_q8CX8wBxolu1djzl4ZTVbvTEl9dvGqRQfQDc";
+
+  window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+</script>
