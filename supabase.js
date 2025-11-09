4375d503d109615ee23617bf9b91e58ccad10ddc
Panbers <56555723+Panbers@users.noreply.github.com>
2025-11-09 16:27:13 -0300

    0911

--- supabase.js
+++ supabase.js
@@ -4,8 +4,7 @@
 
 // Seu projeto Supabase (dados fornecidos por você)
 const SUPABASE_URL = "https://mqlhexjnvmjzxpyzmzur.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xbGhleGpudm1qenhweXptenVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NjI3NjcsImV4cCI6MjA3ODAzODc2N30.CGI5Sizgbi5egZkEyMpeNQvLOcV_hLsJyBNc9J10vEw";
+const SUPABASE_ANON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xbGhleGpudm1qenhweXptenVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NjI3NjcsImV4cCI6MjA3ODAzODc2N30.CGI5Sizgbi5egZkEyMpeNQvLOcV_hLsJyBNc9J10vEw";
 
 // Inicializa o cliente globalmente
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
